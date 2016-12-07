f9b5b42fddbb431c1cc26faafe9b306a65b58c67
xxx222 <1390357620@qq.com>
2016-12-07 11:41:38 +0800

    Update MainActivity.java

--- src/com/example/test3/MainActivity.java
+++ src/com/example/test3/MainActivity.java
@@ -12,6 +12,8 @@ TextView tv2;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    
+    zhangkaiyushigou
     }
 
 
